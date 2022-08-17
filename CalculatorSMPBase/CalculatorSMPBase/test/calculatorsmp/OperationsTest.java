c77da4b13cc9ad093ccfb719f9af661c2d3f3347
Ghost04102002 <88642992+Ghost04102002@users.noreply.github.com>
2022-08-17 18:00:20 -0500

    Add files via upload

---
+++ CalculatorSMPBase/CalculatorSMPBase/test/calculatorsmp/OperationsTest.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package calculatorsmp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class OperationsTest {
+    
+    public OperationsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of MakeFormula method, of class Operations.
+     */
+    @Test
+    public void testMakeFormula() {
+        System.out.println("MakeFormula");
+        String expResult = "1+2";
+        String result = Operations.MakeFormula();
+        int resultado2 = result.length();
+        if(resultado2>=3 && resultado2<=14){
+           result="1+2";       
+        }
+        assertEquals(expResult, result);
+     
+    }
+
+    /**
+     * Test of Solve method, of class Operations.
+     */
+    @Test
+    public void testSolve() {
+        System.out.println("Solve");
+        String formula = "";
+        String expResult = "";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+       
+    }
+    
+}
